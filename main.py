dc2ed45fcbd6c8a99487e4da3215a1ec9f9b5b10
angelm1974 <18185521+angelm1974@users.noreply.github.com>
2021-12-18 11:19:37 +0100

    z warunkami

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 # tu będzie gra kółko i krzyżyk
 
-PlanszaDoGry ={'7':' ','8':' ','9':' ',
-               '4':' ','5':' ','6':' ',
-               '1':' ','2':' ','3':' '}
+PlanszaDoGry = {'7': ' ', '8': ' ', '9': ' ',
+                '4': ' ', '5': ' ', '6': ' ',
+                '1': ' ', '2': ' ', '3': ' '}
 
-klawiszeGry=[]
+klawiszeGry = []
 
 for key in PlanszaDoGry:
     klawiszeGry.append(key)
+
 
 def drukujPlansze(pole):
     print(f"{pole['7']}|{pole['8']}|{pole['9']}")
@@ -16,5 +17,72 @@ def drukujPlansze(pole):
     print('-+-+-')
     print(f"{pole['1']}|{pole['2']}|{pole['3']}")
 
-drukujPlansze(PlanszaDoGry)
-#Testowy komentarz
+
+def gra():
+
+    gracz = 'X'
+    licznik = 0
+
+    for i in range(10):
+        drukujPlansze(PlanszaDoGry)
+
+        move = input(
+            f'To jest ruch, {gracz}. Wybierz gdzie chcesz postawić znak!')
+
+        if PlanszaDoGry[move] == ' ':
+            PlanszaDoGry[move] = gracz
+            licznik += 1
+        else:
+            print('Miejsce jest już zajęte!!!\nWstaw swój znak gdzieś indzej!')
+            continue
+
+        if licznik >= 5:
+            if PlanszaDoGry['7']==PlanszaDoGry['8']==PlanszaDoGry['9']!= ' ':
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!\n')
+                print(f'WYGRAŁ GRACZ: {gracz}')
+                break
+            
+            elif PlanszaDoGry['4']==PlanszaDoGry['5']==PlanszaDoGry['6']!= ' ':
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!\n')
+                print(f'WYGRAŁ GRACZ: {gracz}')
+                break
+            
+            elif PlanszaDoGry['1']==PlanszaDoGry['2']==PlanszaDoGry['3']!= ' ':
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!\n')
+                print(f'WYGRAŁ GRACZ: {gracz}')
+                break
+            #tutaj kończą się wygrane poziome
+            
+            elif PlanszaDoGry['7']==PlanszaDoGry['4']==PlanszaDoGry['1']!= ' ':
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!\n')
+                print(f'WYGRAŁ GRACZ: {gracz}')
+                break
+
+            elif PlanszaDoGry['8']==PlanszaDoGry['5']==PlanszaDoGry['2']!= ' ':
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!\n')
+                print(f'WYGRAŁ GRACZ: {gracz}')
+                break
+
+            elif PlanszaDoGry['9']==PlanszaDoGry['6']==PlanszaDoGry['3']!= ' ':
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!\n')
+                print(f'WYGRAŁ GRACZ: {gracz}')
+                break
+            
+#wygrane przekątne
+            elif PlanszaDoGry['7']==PlanszaDoGry['5']==PlanszaDoGry['3']!= ' ':
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!\n')
+                print(f'WYGRAŁ GRACZ: {gracz}')
+                break
+            
+            elif PlanszaDoGry['9']==PlanszaDoGry['5']==PlanszaDoGry['1']!= ' ':
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!\n')
+                print(f'WYGRAŁ GRACZ: {gracz}')
+                break
